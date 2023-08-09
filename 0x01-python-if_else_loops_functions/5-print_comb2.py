4c12bb9864f53bbd2505882d068b8e08858f5eb5
WAYLANbruintjies <waylandyzz632@gmail.com>
2023-08-09 12:21:08 +0000

    Error and Bugs fixes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for numbers range (0, 100):
-	if numbers == 99:
-		print("{}".format(numbers))
-	else:
-		print("{:02}".format(numbers), end=", ")
+for numbers in range (0, 100):
+    if numbers == 99:
+        print("{}".format(numbers))
+    else:
+        print("{:02}".format(numbers), end=", ")
