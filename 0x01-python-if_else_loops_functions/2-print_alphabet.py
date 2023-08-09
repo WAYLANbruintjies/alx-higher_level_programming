ab282aadcd3a5e8966023150623473a1e83cc6d5
WAYLANbruintjies <waylandyzz632@gmail.com>
2023-08-09 11:44:16 +0000

    Error fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@
 Use the ASCII alphabet, print chars in lower case, not followed by a new line.
 '''
 for alpha in range(97, 123):
-	print("{}".format(chr(alpha)), end="")
+    print("{}".format(chr(alpha)), end="")
