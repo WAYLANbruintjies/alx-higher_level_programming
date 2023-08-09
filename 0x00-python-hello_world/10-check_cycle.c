969ab7d017afe9c6834d746273b95c420b0403da
WAYLANbruintjies <waylandyzz632@gmail.com>
2023-08-09 20:53:25 +0000

    Updated and modified files to executables

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks to see if a linked list has a cycle
+ * @list: the linked list to be computed
+ * Return: 1 for cycle, 0 for no cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *low = list;
+	listint_t *high = list;
+
+	if (!list) /* if list is empty */
+		return (0);
+
+	while (low && high && high->next)
+	{
+		low = low->next;
+		high = high->next->next;
+		if (low == high)
+			return (1);
+	}
+
+	return (0);
+}
