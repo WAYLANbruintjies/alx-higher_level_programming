2b32d0ed80cb1aa6bc5fb66f17a3fca644387009
WAYLANbruintjies <waylandyzz632@gmail.com>
2023-08-11 10:55:46 -0700

    Modified & Updated

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    infinite = 0
+    for i in range(len(sys.argv) - 1):
+        infinite += int(sys.argv[i + 1])
+        print("{}".format(infinite))
