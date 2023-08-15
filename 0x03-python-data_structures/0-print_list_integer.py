4d564587dbf168783801989100e54212f60ed263
WAYLANbruintjies <waylandyzz632@gmail.com>
2023-08-15 07:56:25 -0700

    Bugs & fixes (update)

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 #Prints all integers of a list
 
 def print_list_integer(my_list=[]):
-    for n in range(len(my_list))
-    print("{:d}".format(my_list[n]))
+    for i in range(len(my_list))
+    print("{:d}".format(my_list[i]))
