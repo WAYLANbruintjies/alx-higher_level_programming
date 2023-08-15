7ae09e69fae8acaa84b52a96f062a44b5f037870
WAYLANbruintjies <waylandyzz632@gmail.com>
2023-08-15 14:08:52 -0700

    Files updated

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - Prints basic info about Python lists
+ * @p: PyObject list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, space, x;
+	PyObject *ob;
+
+	size = Py_SIZE(p);
+	space = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", space);
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Element %d: ", x);
+
+		ob = PyList_GetItem(p, x);
+		printf("%s\n", Py_TYPE(ob)->tp_name);
+	}
+}
