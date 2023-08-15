f47b9a1f83b9f2b7c4274ce5e213ec2cbe6f0638
WAYLANbruintjies <waylandyzz632@gmail.com>
2023-08-15 13:45:34 -0700

    Updated

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    multiples = []
     for num in range(len(my_list)):
         if my_list[num] % 2 == 0:
             multiples.append(True)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,5 +7,6 @@ def max_integer(my_list=[]):
     biggest = my_list[0]
     for n in range(len(my_list)):
         if my_list[n] > biggest:
-            biggest = my_list[n]
+            if my_list[n] > biggest:
+                biggest = my_list[n]
         return (biggest)
