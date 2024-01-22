2548ca534476e31b0cd573d7a740ceb3737cca50
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-01-22 10:29:40 -0800

    5-no_c.py file edited and modified (bug fix)

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 
 def no_c(my_string):
     """A function that removes all characters c and C from a string."""
-     if my_string[]:
+     if my_string[:]:
          new = my_string.translate({ord ("c"): None})
          alt_string = new.translate({ord ("c"): None})
          return alt_string
