d71fe5514c219c1fda7430822fead39c8008ed5e
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-01-21 23:31:19 -0800

    Error/Bug fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#Prints all integers of a list
+# File: 0-print_list_integer.py
 
 def print_list_integer(my_list=[]):
-    """0. Print a list of integers"""
+    """Prints a list of all the integers within the list"""
     for n in range(len(my_list)):
         print("{:d}".format(my_list[n]))
