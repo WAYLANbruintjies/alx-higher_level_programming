b90cd494ea21456f472bf088ed404be9735e3042
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-01-22 11:18:44 -0800

    7. Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,16 +2,9 @@
 # File: 7-add_tuple.py
 
 def add_tuple(tuple_a=(), tuple_b=()):
-     """A fiunction that adds 2 tuples."""
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 0:
-            tuple_b = 0, 0
-        else:
-            tuple_b = tuple_b[0], 0
-
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    """A fiunction that adds 2 tuples."""
+    tuple_new = ()
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    tuple_new = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return tuple_new
