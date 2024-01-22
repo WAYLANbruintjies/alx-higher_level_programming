91fcd0e601be091af5fa22492a2313579dbb2409
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-01-21 22:07:50 -0800

    Modified file for correction

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,4 +4,4 @@
 def print_list_integer(my_list=[]):
     """0. Print a list of integers"""
     for n in range(len(my_list)):
-        print("{}".format(my_list[n]))
+        print("{:d}".format(my_list[n]))
