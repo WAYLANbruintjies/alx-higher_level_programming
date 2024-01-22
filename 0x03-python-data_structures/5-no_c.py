dfa1166a6afb769dbe11c8aa4866dfc1b5955942
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-01-22 10:38:37 -0800

    Code error fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,6 @@ def no_c(my_string):
     """A function that removes all characters c and C from a string."""
      if my_string[:]:
          new = my_string.translate({ord ("c"): None})
-         alt_string = new.translate({ord ("c"): None})
+         alt_string = new.translate({ord ("C"): None})
          return alt_string
      return my_string
