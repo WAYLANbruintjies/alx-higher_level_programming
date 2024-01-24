155973b21a9a884fee45c88135348493c7fa9e08
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-01-24 20:40:35 +0000

    1. Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""1. Search and replace"""
+#File: 1-search_replace.py
+
+"""A function that replaces all occurrences of an element by another in a new list"""
 def search_replace(my_list, search, replace):
     _new_list = list(map(lambda x: replace if x == search else x, my_list))
     return (_new_list)
