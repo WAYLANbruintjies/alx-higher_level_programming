093a8e2f8d4dcf6cc05a9cdae7d1562ecb9df7ae
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-01-24 20:56:09 +0000

    2. Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-#File: 2-uniq_add.py
 
-"""function that adds all unique integers in a list (only once for each integer)"""
 def uniq_add(my_list=[]):
     uni_list = set(my_list)
     nums = 0
