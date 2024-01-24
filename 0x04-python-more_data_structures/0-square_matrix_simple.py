90e641d3aa4632ecdc6f41f702e00a35b0e7fe45
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-01-24 21:01:00 +0000

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-#0-square_matrix_simple.py
 
 def square_matrix_simple(matrix=[]):
-    """A function that computes the square value of all integers of a matrix"""
     new_matrix = matix.copy()
 
     for i in range(len(matrix)):
