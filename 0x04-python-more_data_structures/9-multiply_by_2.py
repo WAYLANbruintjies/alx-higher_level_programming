d8930e1ef60ae2df5544d077f7d6ffedca51177a
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-01-25 02:23:00 +0000

    Multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 def multiply_by_2(a_dictionary):
-    a_dir = a_dictionary.copy()
-    key_list = list(a_dir.keys())
+    b_dictionary = a_dictionary.copy()
+    key_list = list(b_dictionary.keys())
 
     for i in key_list:
-        a_dir[i] *= 2
+        b_dictionary[i] *= 2
 
-    return (a_dir)
+    return (b_dictionary)
