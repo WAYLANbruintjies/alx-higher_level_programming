611951f8cd443ba44042a7b3b0b674e63e7e3f1e
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-06 12:28:50 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Initialise a new Square"""
+
+        self.__size = size
+
