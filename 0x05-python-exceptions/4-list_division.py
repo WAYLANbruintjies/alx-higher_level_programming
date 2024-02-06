76f8cfc10206d11158695d9e92fcb10bc49e4471
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-06 02:47:30 -0800

    Divide two lists element by element

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+
 def list_division(my_list_1, my_list_2, list_length):
-"""Divides two lists element by element
+    """Divides two lists element by element
 
     Args:
         my_list_1 (list): First list
