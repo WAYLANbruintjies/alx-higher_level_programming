48789b53d9ba6c17c6d4b6d3f9249a36588b2d7e
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-13 06:37:15 -0800

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-1. Real definition of a rectangle
+2. Area and Perimeter
 """
 
 
@@ -9,7 +9,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """
-        Initializes Rectangle class instance within constructor
+        Initializes a Rectangle
         """
         self.width = width
         self.height = height
@@ -21,9 +21,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """
-        Sets the width of a Rectangle > 0
-        """
+        """Sets the width of a Rectangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -32,16 +30,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """
-        Retrieves the height of a Rectangle
-        """
+        """Retrieves the height of a Rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Sets the height of a Rectangle > 0
-        """
+        """Sets the height of a Rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -60,16 +54,17 @@ class Rectangle:
                 new_str += '#'
             new_str += '\n'
         return new_str[:-1]
-    
+
     def area(self):
         """
-        Calculates the area of a Rectangle- height * width
+        Calculates the area of a Rectangle
+            Area = height * width
         """
         return self.__width * self.__height
 
     def perimeter(self):
-        """
-        Compute the perimeter of a Rectangle- 2 * (height + width)
+        """Calculates the perimeter of a Rectangle
+            Perimeter = 2 * (height + width)
         """
         if self.__height == 0 or self.__width == 0:
             return 0
