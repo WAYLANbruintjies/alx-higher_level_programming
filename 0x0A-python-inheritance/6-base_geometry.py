156e92b542f40f0463fee1803eb5818dae075a2e
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-19 13:44:27 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""6. Improve Geometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """Raise exception, not implemented"""
+        raise Exception("area() is not implemented")
