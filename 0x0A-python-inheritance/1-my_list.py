5feb5e53c8cb455674d5718404007787f303561e
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-19 09:47:39 -0800

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""1. My list, File: 1-my_list.py"""
+
+
+class MyList(list):
+    """list that inherits MyList"""
+
+    def print_sorted(self):
+        """Prints the list in ascending sort"""
+        print(sorted(list))
