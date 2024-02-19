a44823962e76651bbb4482bd5b39feb341485ece
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-19 03:26:11 -0800

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3 
-
 """0. Lookup, File: 0-lookup.py"""
 
 
 def lookup(obj):
-    """
-    Returns the list of available attributes
-    and methods of an object
-    """
+    """Returns a list of available methods & attributes"""
     return (dir(obj))
