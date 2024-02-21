b22f1e3b2fa062bf88b0c7a0b51ca61612feb998
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-21 00:37:35 -0800

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout
     """
-    with open("my_file_0.txt", "r", encode="utf-8") as f:
-        return(f)
+    with open(filename, mode='r', encoding='utf-8') as f:
+        print(f.read(), end='')
