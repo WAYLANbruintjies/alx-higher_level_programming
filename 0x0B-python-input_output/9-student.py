ef953ae215d552463b95ed9f89953e80f1971eb8
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-21 03:54:46 -0800

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""9. Student to JSON"""
+
+
+class Student():
+    """
+    A student class with public instances
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
+        return self.__dict__
