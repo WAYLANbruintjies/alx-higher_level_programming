c7eb02edd4dcb3c704eb850a6080c24f3c0a4963
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-21 01:44:02 -0800

    From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""4. From JSON string to Object"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python data struct) represented by a JSON string
+    """
+    return json.load(my_str)
