a4ffdd6c31329e09c59954ac9fee63230d7256cb
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-29 07:44:44 -0800

    1. Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""1. Base class"""
+
+
+class Base():
+    """A class called Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Class constructor"""
+        self.id = id
+
+        if id is not None:
+            return id
+        else:
+            __nb_objects += 1
+            id = __nb_objects
