af9417efaeb444bc99cea49d1beaf806d5bd1331
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-03-26 15:19:22 -0700

    Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
 -- Prints the full description of the table 'first_table'
-CREATE TABLE first_table (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n);
 SHOW CREATE TABLE first_table;
