b4952863428cb41facaf5c1ab792aa084c397cfa
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-03-27 14:04:29 -0700

    SQL Updated

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,5 @@
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
 GRANT ALL PRIVILEGES
 ON *.*
-TO user_0d_1@localhosit;
+TO user_0d_1@localhosit
+IDENTIFIED BY user_0d_1_pwd;
