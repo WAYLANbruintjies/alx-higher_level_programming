0b4e1195f6651d2214bafa4cfdff0629f432057f
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-03-27 14:11:39 -0700

    SQL Updated

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
 -- Creates a MySQL server user caled 'user_0d_1' on localhost
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-ON *.*
-TO user_0d_1@localhost
-IDENTIFIED BY user_0d_1_pwd;
-FLUSH PRIVILEGES;
+ON * . *
+TO user_0d_1@localhost;
