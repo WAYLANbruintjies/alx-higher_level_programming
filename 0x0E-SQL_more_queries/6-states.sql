0e0fa3e50765cf90071d2f5501e3a8697f6b74aa
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-03-27 14:25:31 -0700

    SQL Updated

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- Creates a database 'hbtn_0d_usa' and a table 'states' (in DB hbtn_0d_usa) in MySQL
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256)NOT NULL);
