8b5479bc01c807eed06c35772adc782336799c5c
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-23 11:45:56 -0700

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// A script that prints a message depending of the number of arguments passed
+
+const args = process.argv.lenght;
+
+if (args === 2 ) {
+	consolde.log('No Argument');
+} else if (args === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
