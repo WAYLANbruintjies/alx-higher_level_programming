d00cc9d531e6ae542e1afb57149ca68f1b859cb9
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-23 12:13:28 -0700

    More argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// A script that prints the first argument passed to it
+
+const args = process.argv[2];
+
+if (args) {
+	console.log(args);
+} else {
+	console.log('No argument');
+}
