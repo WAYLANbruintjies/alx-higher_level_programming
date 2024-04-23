7c5ef15f1051f977828eeb377f356230e44b408e
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-23 10:59:53 -0700

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A script that prints 3 lines
+const L1 = 'C is fun';
+const L2 = 'Python is cool';
+const L3 = 'JavaScript is amazing';
+
+console.log(L1);
+console.log(L2);
+console.log(L3);
