f5372cc23e4760a3762a3c55bfc6603219de7a99
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-24 03:54:59 -0700

    corrections

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const addMeMaybe = (number, theFunction) => {
-    number++;
-    theFunction(number);
+  number++;
+  theFunction(number);
 };
 
 module.exports.addMeMaybe = addMeMaybe;
