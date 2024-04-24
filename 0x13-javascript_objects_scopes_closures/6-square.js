9bd2075c955544b102b0874b31bb55976666dff5
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-24 15:50:22 -0700

    Updated & modified

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const otherSquare = require('./5-square');
+
+class Square extends otherSquare {
+  charPrint(c) {
+    if (c == null) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+module.exports = Square;
