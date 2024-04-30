0eaf453da11d626d66968369dc9fede1dcf9126f
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 09:20:37 -0700

    modified: file

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""Modules to import"""
-import sys
-
+"""A script that lists all State objects from the database hbtn_0e_6_usa"""
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-inpt = sys.argv
-if len(inpt) < 4:
-    exit(1)
-conn_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-engine = create_engine(conn_str.format(inpt[1], inpt[2], inpt[3]))
-Session = sessionmaker(bind=engine)
 
-Base.metadata.create_all(engine)
+if __name__ == "__main__":
 
-session = Session()
+    """Access to db on MySQL server"""
+    db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3])
+    
+    engine = create_engine(db)
+    Session = sessionmaker(bind=engine)
 
-outpt = session.query(State).order_by(State.id),all()
-for state in outpt:
-    print("{}: {}".format(state.id, state.name))
 
-session.close()
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+    print("{}: {}".format(instance.id, instance.name))
+
+    session.close()
