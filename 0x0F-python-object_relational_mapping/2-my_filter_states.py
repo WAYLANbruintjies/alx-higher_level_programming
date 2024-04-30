8a5050aef220f040ed3f6993a2aef1193e1cbb86
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 06:50:28 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-"""Modules to import to list all states from hbtn_0e_0_usa database"""
+"""
+Modules to import to list all states from hbtn_0e_0_usa database
+that takes in an argument where name matches the argument
+"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
+
     # Connect to MySQL server, get login credentials from cmd arguments
-    db = MySQL.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQL.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset='utf8'
+        )
+
     cursor = db.cursor()
 
     # SQL query to retrieve all states, with format filter and ordered by id
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"\
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC" \
             .format(sys.argv[4]))
     rows = cursor.fetchall()
 
@@ -17,5 +30,6 @@ if __name__ == "__main__":
     for row in rows:
         print(row)
 
-    # Close database connection
+    # Close all connection
+    cursor.close()
     db.close()
