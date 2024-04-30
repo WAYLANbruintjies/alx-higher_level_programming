d3a1620a468cbc3b80da176b0caae2331e21db44
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 08:01:32 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
 
     """MySQL server+db connection/setup instantiated"""
-    db = MySQL.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     
     cursor = db.cursor()
 
@@ -16,8 +16,9 @@ if __name__ == "__main__":
             INNER JOIN states ON states.id=cities.state_id")
 
     rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    if rows is not None:
+        for row in rows:
+            print(row)
 
     cursor.close()
     db.close()
