872e5849c81b717d5f844419209b3cdb4cf4cd00
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 14:46:21 -0700

    corrected

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Modules to import to create class definition to work with SQLAlchemy ORM"""
-from sys import argv
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative_base import declarative
+"""
+A script that defines a City class to work with MySQLAlchemy ORM
+"""
 from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
     """City class"""
+
     __tablename__ = 'cities'
 
-    id = Column(Integer, nullable=False, PrimaryKey=True)
+    id = Column(Integer, primary_key=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
