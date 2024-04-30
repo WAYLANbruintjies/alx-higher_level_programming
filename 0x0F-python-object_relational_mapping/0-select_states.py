9334fddde544be067c080bd27cc4865628b57201
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 02:28:19 -0700

    Updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/pyhton3
 """Modules to import"""
-import mysql.connector
+import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
     # MySQL server connection
-    db = mysql.connector.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
 
     # Execute a MySQL query to fetch all states
