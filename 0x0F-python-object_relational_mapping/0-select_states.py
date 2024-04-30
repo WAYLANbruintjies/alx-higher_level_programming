68b3abd4be9d4639838c21ea3d1c1524f14e76e2
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 00:47:41 -0700

    Contains

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
 
-    # Execute a MySQL query to fetch the states
+    # Execute a MySQL query to fetch all states
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     # Get all the rows from the query result
