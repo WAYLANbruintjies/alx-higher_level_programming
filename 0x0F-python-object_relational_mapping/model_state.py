41ca98465fc250db1a4c9c4a65e34af08f75bd09
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 09:04:08 -0700

    1st state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """
-A script that defines a State class and a Base class to work with MySQLAlchemy ORM
+A script defines a State class and a Base class to work with MySQLAlchemy ORM
 """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Class creation that inherits from Base"""
+    """
+    State class that inherits from Base,
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+    """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
