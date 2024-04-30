75fb80b583d2831856f5dc9556df34b723b8cfbe
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 08:24:29 -0700

    All cities by state UPDATED

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,18 +3,22 @@
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # MySQL server connection initialization
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    
+    """MySQL server + db connection setup initialization"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
     cursor = db.cursor()
 
-    # Execute MySQL queries
-    cursor.execute("SELECT cities.name FROM cities INNER JOIN states ON states.id=cities.state_id
+    """Execute MySQL queries and display results"""
+    cursor.execute("SELECT cities.name FROM cities INNER JOIN states ON states.id=cities.state_id \
             WHERE states.name=%s", (sys.argv[4]))
 
     rows = cursor.fetchall()
     temp = list(row[0] for row in rows)
         print(*temp, sep=", ")
 
-    # Close SQL server connection
+    """Close off all connection"""
+    cursor.close()
     db.close()
