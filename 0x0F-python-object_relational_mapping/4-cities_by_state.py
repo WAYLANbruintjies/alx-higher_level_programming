2ede0b57e24c69e5efcf4758b71ade5c70d8f199
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 08:08:04 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,8 +7,14 @@ import sys
 if __name__ == "__main__":
 
     """MySQL server+db connection/setup instantiated"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+
     cursor = db.cursor()
 
     """SQL query execution and print result"""
