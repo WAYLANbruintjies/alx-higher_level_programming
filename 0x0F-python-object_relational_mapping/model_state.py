9a2f4f4a24add669980c4599f447dd57b4c27e16
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 08:58:53 -0700

    1st state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,8 +8,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Class creation that inherits from Base"""
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
