7a79e58b6a27d09b6fe2f3548180c79de7c3386c
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 05:21:11 -0700

    file: 0-select_states.py UPDATED

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,44 @@
 #!/usr/bin/pyhton3
-"""Modules to import"""
+"""
+Modules to import in order to list all states from the database hbtn_0e_0_usa
+sorted by state.id in ascending order
+"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # MySQL server connection
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    # Execute a MySQL query to fetch all states
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    try:
+        # MySQL server connection
+        db = MySQLdb.connect(
+                host='localhost',
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database
+                charset='utf8')
 
-    # Get all the rows from the query result
-    rows = cursor.fetchall()
-    db.commit()
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
 
-    # Print results
-    for row in rows:
-        print(row)
+        # Cursor instantiation
+        cursor = db.cursor()
 
-    # Close all onnections
-    cursor.close()
-    db.close()
+        # Execute a MySQL query to fetch all states
+        cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+        # Get all the rows from the query result
+        rows = cursor.fetchall()
+
+        # Print results
+        for row in rows:
+            print(row)
+
+        # Close all onnections
+        cursor.close()
+        db.close()
