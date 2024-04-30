a2002277615430c5995c48ec431c350e69ccc049
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-30 06:38:21 -0700

    file updated and corrected

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-"""Modules to import to list all states from hbtn_0e_0_usa database"""
+"""
+Modules to import to list all states from hbtn_0e_0_usa database
+starting with N (upper N) sorted in ascending order by states.id
+"""
 
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # Connect to MySQL server, get login credentials from cmd arguments
-    db = MySQL.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    try:
+        # Connect to MySQL server, get login credentials from cmd arguments
+        db = MySQL.connect(
+                host='localhost',
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                charset='utf8'
+            )
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
+
     cursor = db.cursor()
 
     # SQL query to retrieve all states, where name starts with 'N' and ordered by id
