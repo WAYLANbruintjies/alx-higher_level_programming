81b272684dbd11495c24c9108e2cbb3d10efcde8
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-05-10 12:53:33 -0700

    Python - Network #0 (mandatory)

---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+# Bash script that takes in a URL and displays all HTTP methods the server will accept.
+
+curl -s -I "$1" | grep "Allow" | cut -d " " -f 2-
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Algorithm Technical interview prep - find the peak in an unsorted integer list.
+Prototype: def find_peak(list_of_integers):
+"""
+
+def find_peak(list_of_integers):
+    """Function that finds a peak in a list on unsorted ints"""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
