2d8959afbef517ca9453b6a6afb2549189d167aa
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-05-10 14:49:57 -0700

    Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import urllib.request
+
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.headers["X-Request-Id"])
