719786d816a57a10d7796f5329a822073997df99
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-05-11 07:12:18 -0700

    Updated file: 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,10 +4,12 @@ Python script that takes in a URL and an email, sends a POST request to the pass
 with the email as a parameter, and displays the body of the response (decoded in utf-8)
 """
 
+from sys import argv
+import urllib.request
+import urllib.parse
+
+
 if __name__ == "__main__":
-    from sys import argv
-    import urllib.parse
-    import urllib.request
 
     url = argv[1]
     value = {"email": argv[2]}
