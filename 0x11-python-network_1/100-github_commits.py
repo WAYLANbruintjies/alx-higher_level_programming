4bb9c3dc36d62dacf5c11dad6d66ffa3fba45e1c
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-05-12 05:23:49 -0700

    Interview question (#advanced)

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Lists the 10 most recent commits on a given GitHub repo.
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        argv[2], argv[1])
+
+    res = requests.get(url)
+    commits = res.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
